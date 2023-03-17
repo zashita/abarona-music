519c31a9c984e5bd620911a08551d0a58074f584
zashita <mihail.trutsko@gmail.com>
2023-03-17 13:01:16 +0300

    fixed wrapper issue

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,12 @@
-import React, {FC} from 'react';
-import {Provider} from 'react-redux';
+import React from 'react';
 import {AppProps} from 'next/app';
 import {wrapper} from "@/store";
 
-const MyApp: FC<AppProps> = ({Component, ...rest}) => {
-    const {store, props} = wrapper.useWrappedStore(rest);
-    return (
-        <Provider store={store}>
-            <Component {...props.pageProps} />
-        </Provider>
-    );
-};
+class MyApp extends React.Component<AppProps> {
+    render() {
+        const {Component, pageProps} = this.props;
+        return <Component {...pageProps} />;
+    }
+}
+
+export default wrapper.withRedux(MyApp);
